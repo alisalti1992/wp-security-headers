ecf83f34b70a927c5817f8acc51ac5c015773fc3
moveaheadmedia <ali@moveaheadmedia.co.uk>
2023-01-11 13:36:51 +0700

    Update wp-security-headers.php

--- wp-security-headers.php
+++ wp-security-headers.php
@@ -15,8 +15,7 @@ function mam_wordpress_security_headers() {
 	header( 'X-Frame-Options: SAMEORIGIN' );
 	header( 'X-Content-Security-Policy: default-src \'self\'; script-src \'self\';' );
 	header( 'Strict-Transport-Security: max-age=31536000' );
-	header("Content-Security-Policy: default-src 'self'; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'; img-src * data:; connect-src *; font-src *;");
-	header( 'Referrer-Policy: no-referrer-when-downgrade' );
+	header("Content-Security-Policy: default-src 'self'; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'; img-src * data:; connect-src *; font-src *; frame-src: *;");	header( 'Referrer-Policy: no-referrer-when-downgrade' );
 	header('Permissions-Policy: accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), window-placement=(), vertical-scroll=()');
 }
 add_action( 'send_headers', 'mam_wordpress_security_headers' );
