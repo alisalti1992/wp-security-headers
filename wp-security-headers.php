636f1b9a5605347f3a7a9ab39b688e3a6d413b4d
moveaheadmedia <ali@moveaheadmedia.co.uk>
2023-01-11 13:20:57 +0700

    Create wp-security-headers.php

---
+++ wp-security-headers.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+Plugin Name: WordPress Security Headers
+Plugin URI: https://github.com/moveaheadmedia/wp-security-headers
+Description: A simple plugin to add custom headers to your site
+Version: 1.0.0
+Author: Ali Sal
+Author URI: https://moveaheadmedia.com/
+*/
+
+
+function toptal_wordpress_security_headers() {
+    header( 'X-XSS-Protection: 1; mode=block' );
+    header( 'X-Content-Type-Options: nosniff' );
+    header( 'X-Frame-Options: SAMEORIGIN' );
+    header( 'X-Content-Security-Policy: default-src \'self\'; script-src \'self\';' );
+    header( 'Strict-Transport-Security: max-age=31536000' );
+	header("Content-Security-Policy: default-src 'self'");
+	header( 'Referrer-Policy: no-referrer-when-downgrade' );
+	header('Permissions-Policy: accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), window-placement=(), vertical-scroll=()');
+}
+add_action( 'send_headers', 'toptal_wordpress_security_headers' );
